82aaca323fc186122e8a5eaee9ea04645e93eebb
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-04-09 15:05:44 +0200

    Yeelight sensor ready.

--- azureedge/modules/yeelight-sensor-module.js
+++ azureedge/modules/yeelight-sensor-module.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const Lookup = require('node-yeelight-wifi').Lookup;
+var deepEqual = require('deep-equal');
 
 const EVENT_LIGHT_DETECTED = 'EVENT_LIGHT_DETECTED';
 const EVENT_LIGHT_DISCONNECTED = 'EVENT_LIGHT_DISCONNECTED';
 const EVENT_LIGHT_STATUS_UPDATE = 'EVENT_LIGHT_STATUS_UPDATE';
+
+const oldStates = {};
 
 module.exports = {
   broker: null,
@@ -21,19 +24,22 @@ module.exports = {
 
     let look = new Lookup();
     look.on('detected',(light) => {
-        console.log(`New yeelight detected: ${light.id}.`);
-
         this.publishMessage(EVENT_LIGHT_DETECTED, 
-                       `New yeelight bulb detected: ${light.id}.`);
+                       `New yeelight bulb detected: ${light.id}.`, light);
 
         light.on('stateUpdate',(light) => { 
+          // TODO: manage scenes (currently no event is fired when the rgb is set by a scene)
+          const currentState = light.getState();
+          if (!oldStates[light.id] || !deepEqual(oldStates[light.id], currentState)) {
             this.publishMessage(EVENT_LIGHT_STATUS_UPDATE, 
-                        `Yeelight bulb state changed: ${light.id}.`);
+              `Yeelight bulb state changed: ${light.id}.`, light);
+            oldStates[light.id] = currentState;
+          }
         });
 
         light.on('disconnected',() => { 
-            this.publishMessage(EVENT_LIGHT_DISCONNECTED, 
-                        `Lost connection with yeelight bulb ${light.id}.`);
+          this.publishMessage(EVENT_LIGHT_DISCONNECTED, 
+                      `Lost connection with yeelight bulb ${light.id}.`, light);
         });
     
     });
@@ -47,7 +53,36 @@ module.exports = {
     console.log('yeelight-sensor.destroy');
   },
 
-  publishMessage(eventId, text, payload) {
+  /*  Example of generated event:
+      {
+        "eventId": "EVENT_LIGHT_DETECTED",
+        "text": "New yeelight bulb detected: 0x0000000003329cc3.",
+        "payload": {
+          "type": "color",
+          "power": true,
+          "bright": 47,
+          "rgb": {
+            "r": 0,
+            "g": 255,
+            "b": 0
+          },
+          "hsb": {
+            "h": 120,
+            "s": 100,
+            "b": 47
+          }
+        }
+      }
+
+
+  */
+  publishMessage(eventId, text, light) {
+    const payload = light.getState();
+    payload.id = light.id;
+    payload.name = light.name;
+    payload.mac = light.mac;
+    payload.host = light.host;
+    payload.port = light.port;
     const message = { eventId, text, payload };
     const promise = this.broker.publish({
       properties: {
