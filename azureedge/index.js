ba12f73b9f77fd6850580128100864508aee01cc
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-04-09 12:33:12 +0200

    Cannot load module.

---
+++ azureedge/index.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+  
+    const Gateway = require('azure-iot-gateway');
+    let configPath = null;
+  
+    // node app.js [local | cloud ]
+    if (process.argv.length < 3) {
+      throw 'Syntax: npm run [local|cloud]';
+    }
+  
+    const option = process.argv[2];
+  
+    if (option === 'local') {
+      configPath = './gw_local_config.json';
+    } else if (option === 'cloud') {
+      configPath = './gw_cloud_config.json';
+    } else {
+      throw 'Invalid option to start app.js !';
+    }
+  
+    const gw = new Gateway(configPath);
+    gw.run();
+  })();
+  
---
+++ azureedge/modules/yeelight-module-sensor.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const Lookup = require('node-yeelight-wifi').Lookup;
+
+const EVENT_LIGHT_DETECTED = 'EVENT_LIGHT_DETECTED';
+const EVENT_LIGHT_DISCONNECTED = 'EVENT_LIGHT_DISCONNECTED';
+const EVENT_LIGHT_STATUS_UPDATE = 'EVENT_LIGHT_STATUS_UPDATE';
+
+module.exports = {
+  broker: null,
+  configuration: null,
+
+  create: function (broker, configuration) {
+    this.broker = broker;
+    this.configuration = configuration;
+
+    return true;
+  },
+
+  start: function () {
+
+    let look = new Lookup();
+    look.on('detected',(light) => {
+        console.log(`New yeelight detected: ${light.id}.`);
+
+        publishMessage(EVENT_LIGHT_DETECTED, 
+                       `New yeelight bulb detected: ${light.id}.`);
+
+        light.on('stateUpdate',(light) => { 
+            publishMessage(EVENT_LIGHT_STATUS_UPDATE, 
+                        `Yeelight bulb state changed: ${light.id}.`);
+        });
+
+        light.on('disconnected',() => { 
+            publishMessage(EVENT_LIGHT_DISCONNECTED, 
+                        `Lost connection with yeelight bulb ${light.id}.`);
+        });
+    
+    });
+
+  },
+
+  destroy: function() {
+    console.log('sensor.destroy');
+  }
+};
+
+function publishMessage(eventId, text, payload) {
+    const message = { eventId, text, payload };
+    const promise = this.broker.publish({
+      properties: {
+        source: 'yeelight-module-sensor',
+        name: eventId
+      },
+      content: new Uint8Array(Buffer.from(JSON.stringify(message), 'utf8'))
+    });  
+    return promise;                        
+}
