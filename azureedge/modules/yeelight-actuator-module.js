6a0b89baad2ee5d7eff4dd15ac0dfd6dd5cde7a0
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-04-10 21:31:02 +0200

    Implemented on, off, rgb and bright level.

---
+++ azureedge/modules/yeelight-actuator-module.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const Lookup = require('node-yeelight-wifi').Lookup;
+const camelcase = require('camelcase');
+
+const lights = {};
+
+module.exports = {
+  broker: null,
+  configuration: null,
+
+  create: function (broker, configuration) {
+    this.broker = broker;
+    this.configuration = configuration;
+
+    return true;
+  },
+
+  start: function () {
+    console.log('yeelight-actuator.start');
+
+    let look = new Lookup();
+    look.on('detected',(light) => {
+        lights[light.id] = light;
+        light.on('stateUpdate', (light) => {});
+        light.on('disconnected',() => {
+            // TODO: how to know which one?
+            console.log(`At least one light lost connection.`);
+        });
+    });
+  },
+
+  /* 
+    moduleMessage example:
+
+    {
+        "properties": {
+            "source": "iothub-amqp-module",
+            "name": "EVENT_DIRECT_METHOD_INVOKED"
+        },
+        "content": {
+            "0": 123,
+            ...
+            "143": 125,
+            "144": 125
+        }
+    }
+        
+    Decodified moduleMessage.content example:
+
+    {
+        "eventId": "EVENT_DIRECT_METHOD_INVOKED",
+        "text": "Direct method (synchronous) invoked from Azure.",
+        "payload": {
+            "command": "CMD_POWER_ON",
+            "args": []
+        }
+    }    
+  */
+
+  receive(moduleMessage) {
+    if(moduleMessage.content){
+      let data = JSON.parse(Buffer.from(moduleMessage.content).toString('utf8'));
+      const commandName = camelcase(data.payload.command);
+      const commandFn = this[commandName];
+      if (!commandFn) {
+        console.error(`Command ${commandName} not supported.`);
+      } else {
+        commandFn.apply(this, data.payload.args);
+      }
+    } else {
+      console.log('yeelight-actuator.receive - Empty Message.content.');
+    }
+  },
+
+  cmdPowerOn() {
+    for (let lightId in lights) {
+        if (lights.hasOwnProperty(lightId)) {
+            lights[lightId].setPower('on', 0.5);
+        }
+    }     
+  },
+
+  cmdPowerOff() {
+    for (let lightId in lights) {
+        if (lights.hasOwnProperty(lightId)) {
+            lights[lightId].setPower('off', 0.5);
+        }
+    }     
+  },
+
+  /* Each value in the range from 0 to 255. */
+  cmdSetRgb(r, g, b) {
+    for (let lightId in lights) {
+        if (lights.hasOwnProperty(lightId)) {
+            lights[lightId]
+                .setRGB([r, g, b])
+                .then(light => console.log(`${lightId} color changed.`))
+                .catch(err => console.warn(`Error changing color: ${err}.`));
+        }
+    }     
+  },
+
+  /* Valid values from 1 to 100. */
+  cmdSetBright(level) {
+    for (let lightId in lights) {
+        if (lights.hasOwnProperty(lightId)) {
+            lights[lightId].setBright(level);
+        }
+    }     
+  },
+
+  destroy: function() {
+    console.log('yeelight-actuator.destroy');
+  },
+
+};
+
