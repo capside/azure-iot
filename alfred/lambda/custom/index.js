24faff7311e1b960d81d48c1e828317b51dc4cb1
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-04-12 13:06:29 +0200

    IotHub integration.

--- alfred/lambda/custom/index.js
+++ alfred/lambda/custom/index.js
@@ -1,6 +1,8 @@
 'use strict';
 var Alexa = require('alexa-sdk');
+var Client = require('azure-iothub').Client;
 
+const EDGE_DEVICE_ID = 'smarthome';
   
 exports.handler = function(event, context) {
     var alexa = Alexa.handler(event, context);
@@ -14,19 +16,80 @@ var handlers = {
         console.log('Session started with reason: ' + JSON.stringify(this.event));
     },
     'SwitchIntent': function () {
-        let action = this.event.request.intent.slots.action ? 
-                     this.event.request.intent.slots.action.value : 'Unknown';
-
-        let device = this.event.request.intent.slots.device ? 
+        let alexaDevice = this.event.request.intent.slots.device ? 
                      this.event.request.intent.slots.device.value : 'Unknown';      
+        let alexaAction = this.event.request.intent.slots.action ? 
+                     this.event.request.intent.slots.action.value : 'Unknown';
+ 
+        const iotHubParams = this.alexaToEdgeMapping[alexaDevice][alexaAction];                     
 
-        let message = `Action is ${action} and device is ${device}`; 
-
-        this.response
-            .speak(message)
-            .cardRenderer(message);
-        this.emit(':responseReady');
+        this.invokeIotHubDirectMethod(EDGE_DEVICE_ID, iotHubParams.methodName, iotHubParams.payload, function (err, result) {
+            if (err) {
+                const message = 'Failed to invoke method \'' + message.method.methodName + '\': ' + err.message;
+                console.log(`Error: ${message}.`);
+                this.response
+                    .speak(message)
+                    .cardRenderer(message);
+                this.emit(':responseReady');
+            } else {
+                console.log(`Invoking direct method ${message.method.methodName}`)
+                console.log(message.method.methodName + ' on ' + message.deviceId + ':');
+                console.log(JSON.stringify(result, null, 2));
+                const message = `Ok.`;
+                this.response
+                    .speak(message)
+                    .cardRenderer(message);
+                this.emit(':responseReady');
+            }
+        });
     },
+
+    alexaToEdgeMapping : {
+        'lights' : {
+            'turn on' : {
+                methodName : 'controlYeelight',
+                payload : {
+                    command : 'CMD_POWER_ON', 
+                    args : []
+                }
+            },
+            'turn off' : {
+                methodName : 'controlYeelight',
+                payload : {
+                    command : 'CMD_POWER_OFF', 
+                    args : []
+                }
+            }
+        },
+        'heater' : {
+            'turn on' : {
+                methodName : 'controlSonoffRelay',
+                payload : {
+                    command : 'CMD_POWER_ON', 
+                    args : ['purple']
+                }
+            },
+            'turn off' : {
+                methodName : 'controlSonoffRelay',
+                payload : {
+                    command : 'CMD_POWER_OFF', 
+                    args : ['purple']
+                }
+            }
+        }
+    },
+
+    invokeIotHubDirectMethod(deviceId, methodName, payload, callback) {
+        const message = this.convertActionToIotHubMessage(action, device);
+        const iotHubClient = Client.fromConnectionString(connectionString);  
+        const methodParams = {
+            methodName: methodName,
+            payload: payload,
+            timeoutInSeconds: 10
+        };        
+        iotHubClient.invokeDeviceMethod(deviceId, methodParams, callback);        
+    },
+
     'SessionEndedRequest' : function() {
         console.log(`Alfred session ended with reason: ${this.event.request.reason}.`);
     },
