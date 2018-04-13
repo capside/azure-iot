eaf72477de96352fbe1294bac1e3d2b5878a3fde
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-04-13 11:09:38 +0200

    Completed initial features.

--- alfred/lambda/custom/index.js
+++ alfred/lambda/custom/index.js
@@ -12,6 +12,58 @@ exports.handler = function(event, context) {
     alexa.execute();
 };
 
+const alexaToEdgeMapping = {
+    'illumination' : {
+        'turn on' : {
+            methodName : 'controlYeelight',
+            payload : {
+                command : 'CMD_POWER_ON', 
+                args : []
+            },
+            response : `Ok, %s. The illumination has been turned on.` 
+        },
+        'turn off' : {
+            methodName : 'controlYeelight',
+            payload : {
+                command : 'CMD_POWER_OFF', 
+                args : []
+            },
+            response : `No problem, %s. I've turned off the illumination.` 
+        }
+    },
+    'heater' : {
+        'turn on' : {
+            methodName : 'controlSonoffRelay',
+            payload : {
+                command : 'CMD_POWER_ON', 
+                args : ['purple']
+            },
+            response : `Sure, %s. The heater is on.`
+        },
+        'turn off' : {
+            methodName : 'controlSonoffRelay',
+            payload : {
+                command : 'CMD_POWER_OFF', 
+                args : ['purple']
+            },
+            response : `Of course, %s. The heater is off now.`
+        }
+    }
+};
+
+
+function invokeIotHubDirectMethod(deviceId, methodName, payload, callback) {
+    const connectionString = process.env.IotHubConnectionString;
+    console.log(`Executing direct method ${methodName} over ${deviceId}.`);
+    const iotHubClient = Client.fromConnectionString(connectionString);  
+    const methodParams = {
+        methodName: methodName,
+        payload: payload,
+        timeoutInSeconds: 10
+    };        
+    iotHubClient.invokeDeviceMethod(deviceId, methodParams, callback);        
+}
+
 var handlers = {
     'LaunchRequest': function () {
         console.log('Session started with reason: ' + JSON.stringify(this.event));
@@ -22,73 +74,42 @@ var handlers = {
         let alexaAction = this.event.request.intent.slots.action ? 
                      this.event.request.intent.slots.action.value : 'Unknown';
  
-        const iotHubParams = this.alexaToEdgeMapping[alexaDevice][alexaAction];                     
+        const targetCommands = alexaToEdgeMapping[alexaDevice];                     
+        if (!targetCommands) {
+            const message = `I'm afraid I don't know what is a ${alexaDevice}.`
+            this.response
+                .speak(message)
+                .cardRenderer(message);
+            this.emit(':responseReady');
+            return;
+        }
+        const iotHubParams = alexaToEdgeMapping[alexaDevice][alexaAction];                     
+        if (!iotHubParams) {
+            const message = `I'm afraid I don't know how to do ${alexaAction} over ${alexaDevice}.`
+            this.response
+                .speak(message)
+                .cardRenderer(message);
+            this.emit(':responseReady');
+            return;
+        }
 
-        this.invokeIotHubDirectMethod(EDGE_DEVICE_ID, iotHubParams.methodName, iotHubParams.payload, function (err, result) {
+        invokeIotHubDirectMethod(EDGE_DEVICE_ID, iotHubParams.methodName, iotHubParams.payload, function (err, result) {
             if (err) {
-                const message = 'Failed to invoke method \'' + message.method.methodName + '\': ' + err.message;
+                const message = 'Failed to invoke method \'' + iotHubParams.methodName + '\': ' + err.message;
                 console.log(`Error: ${message}.`);
                 this.response
                     .speak(message)
                     .cardRenderer(message);
                 this.emit(':responseReady');
             } else {
-                console.log(`Invoking direct method ${message.method.methodName}`)
-                console.log(message.method.methodName + ' on ' + message.deviceId + ':');
-                console.log(JSON.stringify(result, null, 2));
-                const message = `Ok.`;
+                const username = Math.random() < 0.8 ? `` : `Master Bruce.`;
+                const message = iotHubParams.response.replace('%s', username);
                 this.response
                     .speak(message)
                     .cardRenderer(message);
                 this.emit(':responseReady');
             }
-        });
-    },
-
-    alexaToEdgeMapping : {
-        'lights' : {
-            'turn on' : {
-                methodName : 'controlYeelight',
-                payload : {
-                    command : 'CMD_POWER_ON', 
-                    args : []
-                }
-            },
-            'turn off' : {
-                methodName : 'controlYeelight',
-                payload : {
-                    command : 'CMD_POWER_OFF', 
-                    args : []
-                }
-            }
-        },
-        'heater' : {
-            'turn on' : {
-                methodName : 'controlSonoffRelay',
-                payload : {
-                    command : 'CMD_POWER_ON', 
-                    args : ['purple']
-                }
-            },
-            'turn off' : {
-                methodName : 'controlSonoffRelay',
-                payload : {
-                    command : 'CMD_POWER_OFF', 
-                    args : ['purple']
-                }
-            }
-        }
-    },
-
-    invokeIotHubDirectMethod(deviceId, methodName, payload, callback) {
-        const message = this.convertActionToIotHubMessage(action, device);
-        const iotHubClient = Client.fromConnectionString(connectionString);  
-        const methodParams = {
-            methodName: methodName,
-            payload: payload,
-            timeoutInSeconds: 10
-        };        
-        iotHubClient.invokeDeviceMethod(deviceId, methodParams, callback);        
+        }.bind(this));
     },
 
     'SessionEndedRequest' : function() {
@@ -112,6 +133,10 @@ var handlers = {
 };
 
 if (isLambda === false) {
+    if (!process.env.IotHubConnectionString) {
+        console.log('Local execution detected but IotHubConnectionString env variable is not set.');
+        return;
+    }
     console.log('Local execution detected. Launching server.');
     var express = require('express');
     var bodyParser = require('body-parser');
