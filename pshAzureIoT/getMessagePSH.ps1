c5db64eb7b86257614a8ddfec458e293c23bc26d
fsolabenitez <fernandosolabenitez@gmail.com>
2018-04-18 23:07:33 +0200

    add code in psh to connect azure iothub and read twin device

---
+++ pshAzureIoT/getMessagePSH.ps1
@@ -0,0 +1,36 @@
+﻿# Set REST API parameters
+$apiVersion = "2016-11-14"
+$contentType = "application/json;charset=utf-8"
+
+# Set HTTP request headers to include Authorization header
+$sasToken = iothub-explorer sas-token "dockerNodejsDevice" --login "HostName=bcampiothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=C9LL7DYPRrzirngFx1cDa1gYmoeim6znp9gIUvoV3zM="
+$count = 0
+Foreach ($sasTokenSingle in $sasToken){
+    if ($count -eq 1){
+        $sas = $sasTokenSingle
+    }
+    $count ++
+}
+$requestHeader = @{"Authorization" = $sas}
+
+# Set initial URI for calling Azure Resource Manager REST API
+$uri = "https://bcampiothub.azure-devices.net/devices/dockerNodejsDevice/messages/deviceBound?api-version=$apiVersion"
+$uriCompleted = "https://bcampiothub.azure-devices.net/devices/dockerNodejsDevice/messages/deviceBound/{etag}?api-version=2016-11-14"
+    
+# Call Azure Resource Manager REST API
+$count = 0
+while ($true){
+    $result = Invoke-RestMethod -Uri $uri -Method Get -Headers $requestHeader -ContentType $contentType
+    
+    if ($result){
+        echo Mensaje recibido: $result
+        $count ++
+        echo $count
+    }
+}
+
+$result = Invoke-RestMethod -Uri $uri -Method Get -Headers $requestHeader -ContentType $contentType
+$result
+
+
+
