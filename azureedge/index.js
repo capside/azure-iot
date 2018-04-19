fcb0e699df9d99c83202b0a787e2bd03d247d46d
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-04-19 16:36:36 +0200

    New options to run the app: npm run dyee

--- azureedge/index.js
+++ azureedge/index.js
@@ -2,23 +2,17 @@
     'use strict';
   
     const Gateway = require('azure-iot-gateway');
-    let configPath = null;
-  
-    // node app.js [local | cloud ]
+    
     if (process.argv.length < 3) {
       throw 'Syntax: npm run [local|cloud]';
     }
 
-    const option = process.argv[2];
-  
-    if (option === 'local') {
-      configPath = './gw_local_config.json';
-    } else if (option === 'cloud') {
-      configPath = './gw_cloud_config.json';
-    } else {
-      throw 'Invalid option to start app.js !';
+    const config = process.argv[2];
+    if (!config) {
+      console.error('Usage: npm run [yee | dyee | cloud | dcloud]');
+      return -1;
     }
-  
+    const configPath = config + '.json';
     const gw = new Gateway(configPath);
     gw.run();
   })();
